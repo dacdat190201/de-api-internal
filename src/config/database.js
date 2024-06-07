999103702cfdc8148e2d205ab696b5a3d192f7fa
dat.nguyend <dat.nguyend@s3corp.com.vn>
2024-06-07 17:02:50 +0700

    connect sql

---
+++ src/config/database.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql2");
+
+export const connectDB = async () => {
+  try {
+    const connection = await mysql.createConnection({
+      host: process.env.HOST_SERVER,
+      port: process.env.PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    });
+    console.log("Connected successfully to the database!");
+    return connection;
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+    throw error;
+  }
+};
--- src/server.js
+++ src/server.js
@@ -1,32 +1,20 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+import express from "express";
+import { connectDB } from "~/config/database";
+import "dotenv/config";
+const startServer = async () => {
+  try {
+    await connectDB();
+    console.log("Connect server");
 
-import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+    const app = express();
+    app.listen(() => {
+      // eslint-disable-next-line no-console
+      console.log("Hello Trung Quan Dev, I am running at");
+    });
+  } catch (err) {
+    console.error(err, "show");
+    process.exit(1); // Use 1 to indicate an error condition
+  }
+};
 
-const app = express()
-
-const hostname = 'localhost'
-const port = 8017
-
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(mapOrder(
-    [ { id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
-
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
-})
+startServer();
